79eb7535d3711ec5fb49a42803f58f55eb4e755c
persianpros <persianpros@yahoo.com>
2021-04-14 15:19:56 +0430

    PEP8 double aggressive E20 and E211

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 pkg = 'Extensions.BhTunerServer'
-setup (name='enigma2-plugin-extensions-bhtunerserver',
+setup(name='enigma2-plugin-extensions-bhtunerserver',
        version='0.1',
        license='GPLv2',
        url='https://github.com/E2OpenPlugins/e2openplugin-BhTunerServer',
